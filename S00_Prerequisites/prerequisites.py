82d3bae893cc25b36214796d78b75ebe88137cf6
bert.gollnick <bert.gollnick@posteo.net>
2024-01-16 18:53:34 +0100

    prerequisites added

---
+++ S00_Prerequisites/prerequisites.py
@@ -0,0 +1,16 @@
+
+
+# %% TODO: write task for function definition
+
+# %% TODO: Create a list of numbers from 0 to 9
+
+#%% TODO: show familiarity with pandas
+
+#%% TODO: show familiarity with numpy
+
+
+
+
+#%% Task: Create a list of squares of the numbers from 0 to 9
+squares = [x**2 for x in range(10)]
+squares
